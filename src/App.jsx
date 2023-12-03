86221a19741351e006669f8d61f92cd8f684286d
Mayank Kumar <icodermayank@gmail.com>
2023-12-03 20:32:05 +0530

    News fetched from NewsAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Navbar from "./Components/Navbar"
-
+import NewsBoard from "./Components/NewsBoard"
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <NewsBoard/>
 
     </div>
   )
